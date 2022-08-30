d91b0c107742174aa4fca587181c422dbd808da0
kyuby13 <kyubyyuby@gmail.com>
2022-08-30 17:02:36 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:1.10.5-alpine3.8
+
+ 
+
+WORKDIR /go/src/app
+
+COPY maingo.go .
+
+RUN go build -o main .
+
+EXPOSE 8000
+
+CMD ["./main"]
---
+++ maingo.go
@@ -0,0 +1,45 @@
+package main
+
+ 
+
+import (
+
+  "fmt"
+
+  "log"
+
+  "net/http"
+
+)
+
+ 
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+  fmt.Fprintf(w, "Hi, this is Docker golang web!")
+
+}
+
+ 
+
+func about(w http.ResponseWriter, r *http.Request) {
+
+  fmt.Fprintf(w, "Hi, this is Docker golang web!")
+
+}
+
+ 
+
+func main() {
+
+  http.HandleFunc("/", index)
+
+  http.HandleFunc("/about", about)
+
+ 
+
+  log.Println("application started at port :8000")
+
+  log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
